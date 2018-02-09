597b1a48dc571d549bd6a0eca54618cc687a71b0
Angie Siu <asiu@mscgroupny.com>
2018-02-09 17:27:13 -0500

    compartmentalize the reducer files

--- src/_reducers/helpers_reducer.js
+++ src/_reducers/helpers_reducer.js
@@ -1,5 +1,4 @@
 import characters_json from '../_data/characters.json';
-import { ADD_CHARACTER } from '../_actions';
 
 export function createCharacter(id) {
 	let character = characters_json.find(char => char.id === id);
