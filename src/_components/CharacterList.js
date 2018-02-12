7050766cdb5bd30b9509973a98727ab3dda0db0a
Angie Siu <asiu@mscgroupny.com>
2018-02-12 09:58:52 -0500

    create a mapDispatchToProps function to bind action creators onto CharacterList component so that it can activate reducers, update store, etc.

--- src/_components/CharacterList.js
+++ src/_components/CharacterList.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux'; // returns another function for us to call
+import { bindActionCreators } from 'redux';
+import { addCharacterById } from '../_actions';
 
 class CharacterList extends React.Component {
 	render() {
+		console.log('this.props', this.props);
+
 		return (
 			<div>
 				<h4>Characters</h4>
@@ -22,12 +26,21 @@ class CharacterList extends React.Component {
 	}
 }
 
+// map our redux state to the props of our component
 function mapStateToProps(state) {
-	console.log('state', state);
-	return {
+	// console.log('state', state);
+	return { // return data to become part of the props in the component
 		characters: state.characters
 	};
 }
 
+function mapDispatchToProps(dispatch) {
+	// bind action creators onto props of the component
+	return bindActionCreators({ addCharacterById }, dispatch);
+}
+
 // connect our component to our redux state
-export default connect(mapStateToProps, null)(CharacterList);
+// pass in the component we want to connect to our redux
+// Param 1: get our redux state as props for our component
+// Param 2: binds our action creators to our props on the component so that it can call action creators, activate reducers, and update the store
+export default connect(mapStateToProps, mapDispatchToProps)(CharacterList);
